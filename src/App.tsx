3edaa1ab73196883f47c6bd661be368a3ac49dea
Reinaldo Dos Santos <reinaldosantos.rs627@gmail.com>
2026-04-30 20:08:14 -0300

    fix: ajustando estrutura de imagens e corrigindo erro de build na Vercel

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,12 @@ import firstBg from './assets/images/first_bg.jpg'
 import secondBg from './assets/images/second_bg.jpg'
 import fourthBg from './assets/images/fourth_bg.jpg'
 import fifthBg from './assets/images/fifth_bg.jpg'
-import sobreImg from './assets/images/sobre.jpg'
+import img1 from './assets/images/sobre/img1.jpg'
+import img2 from './assets/images/sobre/img2.jpg'
+import img3 from './assets/images/sobre/img3.jpg'
+import img4 from './assets/images/sobre/img4.jpg'
 
-const bgImages = [firstBg, secondBg, fourthBg, fifthBg, sobreImg]
+const bgImages = [firstBg, secondBg, fourthBg, fifthBg, img1, img2, img3,img4]
 bgImages.forEach((src) => {
   const img = new Image()
   img.src = src
